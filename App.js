d7853f5d7102b284fae76641b20a77e222242df7
harikirank121 <harikirankaite@gmail.com>
2025-10-16 11:26:54 +0530

    Update App.js with new functionality

--- App.js
+++ App.js
@@ -1,38 +1,5 @@
-var apiKey = 'your_api_key_here'; 
-
-function fetchWeather(city) {
-    var url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey + '&units=metric';
-
-    return fetch(url).then(function (response) {
-        return response.json();
-    }).then(function (data) {
-        return data;
-    }).catch(function (error) {
-        console.log('Error getting weather:', error);
-    });
+function greet() {
+    console.log('Hello from both Feature 1 and Feature 2!');
 }
 
-function displayWeather(data) {
-    console.log('Weather in ' + data.name + ':');
-    console.log('Temperature: ' + data.main.temp + '°C (' + celsiusToFahrenheit(data.main.temp) + '°F)');
-    console.log('Humidity: ' + data.main.humidity + '% (Comfort level: ' + getHumidityComfort(data.main.humidity) + ')');
-    console.log('Description: ' + data.weather[0].description);
-}
-
-function celsiusToFahrenheit(celsius) {
-    
-    return (celsius * 9 / 5) + 32;
-}
-
-function getHumidityComfort(humidity) {
-    
-    if (humidity < 30) {
-        return 'Dry';
-    } else if (humidity < 60) {
-        return 'Comfortable';
-    } else {
-        return 'Humid';
-    }
-}
-
-fetchWeather('London').then(displayWeather);
+greet();
